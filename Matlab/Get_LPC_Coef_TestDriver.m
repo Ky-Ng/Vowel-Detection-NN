2a82095c1061e1bfc07af4d9089cd519a9e6044b
Kyle Ng <kgng@usc.edu>
2024-03-31 23:59:36 -0700

    fix lpcs to correctly use right ammount of coefficients

--- Matlab/Get_LPC_Coef_TestDriver.m
+++ Matlab/Get_LPC_Coef_TestDriver.m
@@ -1,7 +1,8 @@
 function Get_LPC_Coef_TestDriver()
-    x = getLpcCoef([1.0, 2.0, 3.0, 4.0, 5.0], 4, false);
+    addpath("./lib/")
+    x = getLpcCoef([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], 4, false);
     disp(x);
 
-    y = getLpcCoef([1.0, 2.0, 3.0, 4.0, 5.0], 4, true);
+    y = getLpcCoef([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], 4, true);
     disp(y);
 end
--- Matlab/lib/getLpcCoef.m
+++ Matlab/lib/getLpcCoef.m
@@ -1,5 +1,7 @@
 % Wrapper for built-in LPC function with the option to add HIGH_FREQUENCY_REINTRODUCTION Heuristic
 function lpc_coefs = getLpcCoef(audioIn, num_lpc, HIGH_FREQUENCY_REINTRODUCTION)
+    % Add 1 to the num_lpc because the first LPC is always 1.0
+    num_lpc = num_lpc + 1;
     if(HIGH_FREQUENCY_REINTRODUCTION)
         audioIn = diff(audioIn);
     end
